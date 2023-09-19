6924ad70be63286e50bb51daed2ef36debb8ad48
zephyr steiner <esteiner@smith.edu>
2023-09-18 20:35:21 -0400

    modified:   README.md new file:   __pycache__/procedural_resale_shop.cpython-311.pyc modified:   computer.py modified:   oo_resale_shop.py

--- computer.py
+++ computer.py
@@ -1,10 +1,26 @@
 class Computer:
+    description: str = ""
+    processorType: str = ""
+    hardDriveCapacity: int = 0
+    memory: int = 0
+    operatingSystem: str = ""
+    yearMade: int = 0
+    price: int = 0
 
-    # What attributes will it need?
+    def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: int ):
+        self.description = description
+        self.processorType = processor_type
+        self.hardDriveCapacity = hard_drive_capacity
+        self.memory = memory
+        self.operatingSystem = operating_system
+        self.yearMade = year_made
+        self.price = price
+        
+    def updateOS(self, new_os):
+        self.operatingSystem = new_os
+        print(f"Update to {new_os} complete.")
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
+    def updatePrice(self, new_price):
+        self.price = new_price
+        print(f"Updated price to {new_price}.")
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,21 @@
+from typing import Dict, Union, Optional
+
 class ResaleShop:
+    itemID: int = 0
+    inventory : Dict[int, Dict[str, Union[str, int, bool]]] = {}
 
-    # What attributes will it need?
+    def __init__(self, item, inventory):
+        self.itemID = item
+        self.inventory = inventory
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def buy(self, computer):
+        self.itemID += 1
+        self.inventory[self.itemID] = computer
+        return self.itemID
 
+    def sell():
+        pass
+
+    def refurbish():
+        pass
     # What methods will you need?
